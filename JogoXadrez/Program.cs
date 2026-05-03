46f6e504fb6b25ddcdfb3bcbf6c0187bf1e5be33
Jamil Campeão <jamilcampeao@gmail.com>
2026-05-02 22:10:12 -0300

    feat: Cria classe Posição

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace JogoXadrez
+﻿using tabuleiro;
+namespace JogoXadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Posicao p = new Posicao(2, 5);
+
+            Console.WriteLine($"Posição: {p}");
         }
     }
 }
