27b70c494fe4ad014f3d14fa7cff47c975dd46bf
Jamil Campeão <jamilcampeao@gmail.com>
2026-05-02 22:21:23 -0300

    Começo da implementação de um método que imprime o tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,6 +6,8 @@ namespace JogoXadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+            
+            Tela.ImprimirTabuleiro(tab);
         }
     }
 }
---
+++ JogoXadrez/Tela.cs
@@ -0,0 +1,22 @@
+using tabuleiro;
+namespace JogoXadrez;
+
+public class Tela
+{
+    public static void ImprimirTabuleiro(Tabuleiro tab)
+    {
+        for (int i = 0; i < tab.Linha; i++)
+        {
+            for (int j = 0; j < tab.Colunas; j++)
+            {
+                if (tab.Peca(i, j) == null)
+                {
+                    Console.Write("- ");
+                }
+                Console.Write($"{tab.Peca(i, j)} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
